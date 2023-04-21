606ebb1fbb72b6de0b58f6b30366f7cecc2f4a6a
John <johnfox17@gmail.com>
2023-04-21 16:12:16 -0700

    Cleaned up and wrote comments

--- src/heatDiffusionEquation.py
+++ src/heatDiffusionEquation.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#from numpy.linalg import solve
 from scipy.sparse.linalg import inv
 from scipy.sparse import csc_matrix
-import time
 from scipy.sparse import spdiags
 from scipy.integrate import solve_ivp, RK45
 
@@ -14,7 +12,7 @@ L = 10
 dx = 0.1
 dt = 0.001
 t0 = 0
-tf = 2
+tf = 10
 numTimeSteps =int(tf/dt) 
 xCoords = np.arange(-L/2,L/2+dx, dx) #create the discrete x and y grids
 numNodes = len(xCoords)
@@ -34,11 +32,12 @@ def createKMatrix():
     KFD = spdiags(data0, -1, numNodes, numNodes ).toarray()\
             +spdiags(data1, 0, numNodes, numNodes ).toarray()\
             +spdiags(data2, 1, numNodes, numNodes ).toarray()
-    KFD[0][0] = 1 
-    KFD[0][1] = 0
-    KFD[numNodes-1][numNodes-1] = 1
-    KFD[numNodes-1][numNodes-2] = 0
-    return KFD
+    #KFD[0][0] = 1 
+    #KFD[0][1] = 0
+    #KFD[numNodes-1][numNodes-1] = 1
+    #KFD[numNodes-1][numNodes-2] = 0
+    #np.savetxt('/home/doctajfox/Documents/Thesis_Research/heatDiffusionEquation/data/KFD2.csv', KFD[1:numNodes-2,1:numNodes-2], delimiter=",")
+    return KFD[1:numNodes-2,1:numNodes-2]
 
 def calcDuDt(t, initialConditionFD):
     kMat = createKMatrix()
@@ -46,40 +45,64 @@ def calcDuDt(t, initialConditionFD):
     return dudt
 
 def main():
-    #Peridynamics constants
+    ##############################
+    #PDDO Setup
+    ##############################
     numNodes = len(xCoords)
     horizon = 3.015
     delta = horizon * dx
     bVec = np.array([0,0,2])
     diffOrder = 2
     pddo = PDDO.PDDO(numNodes, horizon, bVec, diffOrder, dx)
-    #Initial Condition
-    initialCondition = calcInitialCondition()
-    initialConditionPDDO = initialCondition
-    initialConditionFD = initialCondition
-
     #PDDO
     pddo.findFamilyMembers(xCoords)
     pddo.calcXis(xCoords)
     pddo.calcSysMatrix(kappa)
-    pddo.adjustSysMatrixBoundaryNodes()
+    #pddo.adjustSysMatrixBoundaryNodes()
 
+    ##############################
+    #Initial Condition
+    ##############################
+    initialCondition = calcInitialCondition()
+    initialConditionPDDO = initialCondition
+    initialConditionFD = initialCondition[1:numNodes-2]
+
+    
+    ###############################
+    #Solving with PDDO
+    ###############################
     identity = np.identity(numNodes)
-    
+
     KInvPDDO = inv(csc_matrix(np.identity(pddo.numNodes)-np.multiply(dt,pddo.sysMatrix))).toarray()
-    
+
     for iTimeStep in range(numTimeSteps):
         initialConditionPDDO = np.matmul(KInvPDDO, initialConditionPDDO)
+        initialConditionPDDO[0] = 0
+        initialConditionPDDO[numNodes-1] = 0
         if iTimeStep == numTimeSteps-1:
             SOL_PDDO = initialConditionPDDO
-    
+
+    ###############################
+    #Solving with FD
+    ###############################
     SOL_FD = solve_ivp(calcDuDt, [t0,tf], initialConditionFD, RK45)
-    #Calculate error between FD and PDDO
+    
+    ###############################
+    #Calculating absolute error
+    ##############################
     RK_timeSteps = len(SOL_FD.t)
-    absError = np.abs(np.subtract(SOL_FD.y[:,RK_timeSteps-1],SOL_PDDO))
+    
+    SOL_FD_tf = np.zeros(numNodes)
+    SOL_FD_tf[1:numNodes-2] = SOL_FD.y[:,RK_timeSteps-1] 
+
+    absError = np.abs(np.subtract(SOL_FD_tf,SOL_PDDO))
+    
+    ###############################
+    #Plotting
+    ###############################
     figure, axis = plt.subplots(2,1)  
     axis[0].plot(xCoords, initialCondition, label='Initial Condition')
-    axis[0].plot(xCoords, SOL_FD.y[:,RK_timeSteps-1], marker='o',label='FD')
+    axis[0].plot(xCoords, SOL_FD_tf, marker='o',label='FD')
     axis[0].plot(xCoords, SOL_PDDO, marker='*', label='PDDO')
     axis[0].legend()
     axis[0].grid()
@@ -92,7 +115,6 @@ def main():
     axis[1].set_xlabel('x-axis')
     axis[1].set_ylabel('Absolute Error')
     axis[1].set_title('Absolute Error FD vs PDDO (1.5 sec)')
-
 
 
     plt.show()
