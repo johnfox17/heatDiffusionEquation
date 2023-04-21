164da0ddc4794a98643e349b89e342978600b54b
John <johnfox17@gmail.com>
2023-04-21 14:56:49 -0700

    Modified heatDiffusionEquation.py to use PDDO class and object

--- src/heatDiffusionEquation.py
+++ src/heatDiffusionEquation.py
@@ -1,100 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.neighbors import KDTree
-from numpy.linalg import solve
+#from numpy.linalg import solve
 from scipy.sparse.linalg import inv
 from scipy.sparse import csc_matrix
 import time
 from scipy.sparse import spdiags
 from scipy.integrate import solve_ivp, RK45
 
+import PDDO as PDDO
 
 #Defining constants
 L = 10
 dx = 0.1
 dt = 0.001
 t0 = 0
-tf = 1.5
+tf = 2
 numTimeSteps =int(tf/dt) 
 xCoords = np.arange(-L/2,L/2+dx, dx) #create the discrete x and y grids
 numNodes = len(xCoords)
 kappa = 0.04
-#Peridynamics constants
-horizon = 3.015
-delta = horizon * dx
-bVec = np.array([0,0,2])
-diffOrder = 2
-##################################################################################################
-#PDDO
-##################################################################################################
-def findFamilyMembers():
-    tree = KDTree(xCoords.reshape((numNodes,1)), leaf_size=2)
-    familyMembers = tree.query_radius(xCoords.reshape((numNodes,1)), r = delta)
-    return familyMembers
-
-def calcXis(familyMembers):
-    xis = []
-    for iNode in range(numNodes):
-        family = familyMembers[iNode]
-        currentXis = []
-        for iFamilyMember in range(len(family)):
-            currentXis.append(xCoords[family[iFamilyMember]] - xCoords[iNode])
-        xis.append(currentXis)
-    return xis
-
-def calcSysMatrix(familyMembers, xis):
-    sysMatrix = np.zeros([numNodes,numNodes])
-    #Differential Equation Part
-    for iNode in range(numNodes):
-        family = familyMembers[iNode]
-        xi = xis[iNode]
-        diffMat = np.zeros([3,3])
-        for iFamilyMember in range(len(family)):
-            currentFamilyMember = family[iFamilyMember]
-            currentXi = xi[iFamilyMember]
-            pList = np.array([1, currentXi, (currentXi)**2])
-            weight = np.exp(-4*(np.absolute(currentXi))**2)
-            diffMat += weight*np.outer(pList,pList)*dx
-        for iFamilyMember in range(len(family)):
-            currentFamilyMember = family[iFamilyMember]
-            currentXi = xi[iFamilyMember]
-            pList = np.array([1, currentXi, (currentXi)**2])
-            weight = np.exp(-4*(np.absolute(currentXi))**2);
-            sysMatrix[iNode][ currentFamilyMember] = kappa*weight*np.inner(solve(diffMat,bVec),pList)*dx
-
-    #Boundary Condition 
-    sysMatrix[0][0] = 1
-    sysMatrix[0][1] = 0
-    sysMatrix[0][2] = 0
-    sysMatrix[0][3] = 0
-    sysMatrix[numNodes-1][numNodes-1] = 1
-    sysMatrix[numNodes-1][numNodes-2] = 0
-    sysMatrix[numNodes-1][numNodes-3] = 0
-    sysMatrix[numNodes-1][numNodes-4] = 0
-    return sysMatrix
 
 def calcInitialCondition():
     initialCondition = np.zeros(numNodes)
     for iNode in range(numNodes):
         initialCondition[iNode] = np.exp(-0.5*(xCoords[iNode])**2)
     return initialCondition
-
-def adjustSysMatrixBoundaryNodes(sysMatrix):
-    validValues = sysMatrix[3:10,6]
-    sysMatrix[2:9,5] = validValues
-    sysMatrix[1:8,4] = validValues
-    sysMatrix[1:7,3] = validValues[1:7]
-    sysMatrix[1:6,2] = validValues[2:7]
-    sysMatrix[1:5,1] = validValues[3:7]
-    sysMatrix[1:4,0] = validValues[4:7]
-    
-    sysMatrix[numNodes-9:numNodes-2,numNodes-6] = validValues
-    sysMatrix[numNodes-8:numNodes-1,numNodes-5] = validValues
-    sysMatrix[numNodes-7:numNodes-1,numNodes-4] = validValues[0:6]
-    sysMatrix[numNodes-6:numNodes-1,numNodes-3] = validValues[0:5]
-    sysMatrix[numNodes-5:numNodes-1,numNodes-2] = validValues[0:4]
-    sysMatrix[numNodes-4:numNodes-1,numNodes-1] = validValues[0:3]
-    return sysMatrix
 
 
 def createKMatrix():
@@ -116,21 +46,27 @@ def calcDuDt(t, initialConditionFD):
     return dudt
 
 def main():
-
+    #Peridynamics constants
+    numNodes = len(xCoords)
+    horizon = 3.015
+    delta = horizon * dx
+    bVec = np.array([0,0,2])
+    diffOrder = 2
+    pddo = PDDO.PDDO(numNodes, horizon, bVec, diffOrder, dx)
     #Initial Condition
     initialCondition = calcInitialCondition()
     initialConditionPDDO = initialCondition
     initialConditionFD = initialCondition
 
     #PDDO
-    familyMembers = findFamilyMembers()
-    xis = calcXis(familyMembers)
-    sysMatrix = calcSysMatrix(familyMembers, xis)
-    sysMatrix = adjustSysMatrixBoundaryNodes(sysMatrix)
+    pddo.findFamilyMembers(xCoords)
+    pddo.calcXis(xCoords)
+    pddo.calcSysMatrix(kappa)
+    pddo.adjustSysMatrixBoundaryNodes()
 
     identity = np.identity(numNodes)
     
-    KInvPDDO = inv(csc_matrix(np.identity(numNodes)-np.multiply(dt,sysMatrix))).toarray()
+    KInvPDDO = inv(csc_matrix(np.identity(pddo.numNodes)-np.multiply(dt,pddo.sysMatrix))).toarray()
     
     for iTimeStep in range(numTimeSteps):
         initialConditionPDDO = np.matmul(KInvPDDO, initialConditionPDDO)
