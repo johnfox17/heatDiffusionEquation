59f4f1a132a7f7fc7a9814afd81dcc9160b6b287
johnfox17 <johnfox17@gmail.com>
2023-04-27 21:43:20 -0700

    Started FD functionality to FDLeftDirichletRightNeumannBC.py

--- src/FDLeftDirichletRightNeumannBC.py
+++ src/FDLeftDirichletRightNeumannBC.py
@@ -1,5 +1,6 @@
+import numpy as np
 
-
+from scipy.sparse import spdiags
 ##################################################################################################
 #FD
 ##################################################################################################
@@ -9,17 +10,32 @@ class FD:
         self.numNodes = numNodes
         self.dx = dx
 
-    def createKMatrix():
-         data0 = np.array([(numNodes-1)*[1]])
-    data1 = np.array([(numNodes)*[-2]])
-    data2 = np.array([(numNodes)*[1]])
-    KFD = spdiags(data0, -1, numNodes, numNodes ).toarray()\
+    def createKMatrix(self):
+        numNodes = self.numNodes
+        data0 = np.array([(numNodes-1)*[1]])
+        data1 = np.array([(numNodes)*[-2]])
+        data2 = np.array([(numNodes)*[1]])
+        kMat = spdiags(data0, -1, numNodes, numNodes ).toarray()\
             +spdiags(data1, 0, numNodes, numNodes ).toarray()\
             +spdiags(data2, 1, numNodes, numNodes ).toarray()
-    return KFD[1:numNodes-2,1:numNodes-2]
+        return kMat
 
-    def calcDuDt(t, initialConditionFD):
-        kMat = createKMatrix()
+    def enforceBoundaryConditions(self, kMat):
+        kMat[0,:] = 0
+        kMat[0,0] = 1
+        kMat[0,0] = 1
+        print(kMat)
+        a = input('').split(" ")[0]
+        return kMat
+    
+    def calcDuDt(self, t, initialConditionFD):
+        kMat = FD.createKMatrix(self)
+        kMat = FD.enforceBoundaryConditions(self, kMat)
+
+        print(self.numNodes)
+        print(np.shape(kMat))
+        print(kMat)
+        a = input('').split(" ")[0]
         dudt = np.multiply(kappa/dx**2,np.matmul(kMat, initialConditionFD))
-    return dudt
+        return dudt
 
