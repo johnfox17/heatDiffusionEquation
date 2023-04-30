3c212a9678adc73d7be23ff28135d0248da7498f
John <johnfox17@gmail.com>
2023-04-29 21:07:03 -0700

    Implemented Finite Difference algorithm with first node Dirichlet BC and last node Neumann BC

--- src/FDLeftDirichletRightNeumannBC.py
+++ src/FDLeftDirichletRightNeumannBC.py
@@ -1,14 +1,19 @@
 import numpy as np
-
+from scipy.sparse.linalg import inv
+from scipy.sparse import csc_matrix
 from scipy.sparse import spdiags
 ##################################################################################################
 #FD
 ##################################################################################################
 
 class FD:
-    def __init__(self, numNodes, dx):
+    def __init__(self, numNodes, dx, dt, kappa, kappa2, BC):
         self.numNodes = numNodes
         self.dx = dx
+        self.dt = dt
+        self.kappa = kappa
+        self.kappa2 = kappa2
+        self.BC = BC[0]
 
     def createKMatrix(self):
         numNodes = self.numNodes
@@ -18,24 +23,64 @@ class FD:
         kMat = spdiags(data0, -1, numNodes, numNodes ).toarray()\
             +spdiags(data1, 0, numNodes, numNodes ).toarray()\
             +spdiags(data2, 1, numNodes, numNodes ).toarray()
-        return kMat
+        self.kMat = kMat
 
-    def enforceBoundaryConditions(self, kMat):
-        kMat[0,:] = 0
-        kMat[0,0] = 1
-        kMat[0,0] = 1
-        print(kMat)
-        a = input('').split(" ")[0]
-        return kMat
-    
-    def calcDuDt(self, t, initialConditionFD):
-        kMat = FD.createKMatrix(self)
-        kMat = FD.enforceBoundaryConditions(self, kMat)
+    def enforceLeftBoundaryConditions(self, kMat):
+        kMat[0,0] = 1 #Dirichlet
+        kMat[0,1] = 0
+        self.kMat = kMat
 
-        print(self.numNodes)
-        print(np.shape(kMat))
-        print(kMat)
-        a = input('').split(" ")[0]
-        dudt = np.multiply(kappa/dx**2,np.matmul(kMat, initialConditionFD))
-        return dudt
+    def enforceRightBoundaryConditions(self): 
+        numNodes = self.numNodes
+        dx = self.dx
+        kMat = self.kMat
+        kMat[numNodes-1, -2] = 2
+        kMat[numNodes-1, -1] = -2
+        self.kMat = kMat
 
+    def calcDuDt(self):
+        numNodes = self.numNodes
+        dt = self.dt
+        dx = self.dx
+        kappa = self.kappa
+        FD.createKMatrix(self)
+        kMat = np.zeros([numNodes,numNodes])
+        FD.enforceRightBoundaryConditions(self)
+        identity = np.identity(numNodes-1)
+        kMat[1:numNodes,0:numNodes] = np.multiply(kappa/dx**2, np.multiply(dt,self.kMat[1:numNodes,0:numNodes]))
+        kMat[1:numNodes,1:numNodes] = identity - kMat[1:numNodes,1:numNodes]
+        FD.enforceLeftBoundaryConditions(self, kMat)
+        self.dudt = self.kMat
+
+    def enforceBoundaryConditionsRHS(self, initialCondition):
+        dt = self.dt
+        dx = self.dx
+        BC = self.BC
+        kappa = self.kappa
+        kappa2 = self.kappa2
+        initialCondition[-1] = 2*kappa*dt*BC/(kappa2*dx)
+        self.RHS = initialCondition
+
+
+    def solve(self, tf, initialCondition):
+        numNodes = self.numNodes
+        dt = self.dt
+        numTimeSteps =int(tf/dt)
+
+        FD.calcDuDt(self)
+        invDUDT = inv(csc_matrix(self.dudt)).toarray() 
+        FD.enforceBoundaryConditionsRHS(self, initialCondition)
+        RHS = self.RHS
+        
+        for iTimeStep in range(numTimeSteps):
+            RHS = np.matmul(invDUDT, RHS)
+            FD.enforceBoundaryConditionsRHS(self, RHS)
+            if iTimeStep == numTimeSteps-1:
+                SOL_FD = RHS
+            RHS = self.RHS
+
+        #np.savetxt('/home/doctajfox/Documents/Thesis_Research/heatDiffusionEquation/data/solFD.csv', SOL_FD, delimiter=",")
+        #a = input('').split(" ")[0]
+
+
+        self.SOL_FD = SOL_FD
