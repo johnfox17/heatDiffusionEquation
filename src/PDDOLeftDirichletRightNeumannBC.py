b16ff120a3cab39d32735bd1a66f0bcd61894bf7
John <johnfox17@gmail.com>
2023-05-01 19:23:41 -0700

    Refactored PDDO solution to heat equation with left Dirichlet BC and right Neumann BC

--- src/PDDOLeftDirichletRightNeumannBC.py
+++ src/PDDOLeftDirichletRightNeumannBC.py
@@ -1,34 +1,48 @@
-
 import numpy as np
 from sklearn.neighbors import KDTree
 from numpy.linalg import solve
+from scipy.sparse.linalg import inv
+from scipy.sparse import csc_matrix
 
 ##################################################################################################
 #PDDO
 ##################################################################################################
 class PDDO:
-    def __init__(self, numNodes, horizon, bVec, diffOrder, dx, nodesBC, numBC, BC, diffOrderBC, bVecBC):
+    def __init__(self, numNodes, xCoords, dx, dt, kappa, kappa2, horizon, diffOrder, \
+            bVec, numBC, BC, nodesBC, diffOrderBC, bVecBC):
         self.numNodes = numNodes
-        self.horizon = horizon
-        self.bVec = bVec
-        self.diffOrder = diffOrder
+        self.xCoords = xCoords
         self.dx = dx
-        self.nodesBC = nodesBC
-        self.delta = horizon * dx
+        self.dt = dt
+        self.kappa = kappa
+        self.kappa2 = kappa2
+        self.horizon = horizon
+        self.diffOrder = diffOrder
+        self.bVec = bVec
         self.numBC = numBC
         self.BC = BC
+        self.nodesBC = nodesBC
         self.diffOrderBC = diffOrderBC
         self.bVecBC = bVecBC
+        self.delta = horizon * dx
 
-    def findFamilyMembers(self, xCoords):
-        tree = KDTree(xCoords.reshape((self.numNodes,1)), leaf_size=2)
-        familyMembers = tree.query_radius(xCoords.reshape((self.numNodes,1)), r = self.delta)
+    def findFamilyMembers(self):
+        xCoords = self.xCoords
+        numNodes = self.numNodes
+        delta = self.delta
+
+        tree = KDTree(xCoords.reshape((numNodes,1)), leaf_size=2)
+        familyMembers = tree.query_radius(xCoords.reshape((numNodes,1)), r = delta)
         self.familyMembers = familyMembers
 
-    def calcXis(self, xCoords):
+    def calcXis(self):
+        xCoords = self.xCoords
+        numNodes = self.numNodes
+        familyMembers = self.familyMembers
+        
         xis = []
-        for iNode in range(self.numNodes):
-            family = self.familyMembers[iNode]
+        for iNode in range(numNodes):
+            family = familyMembers[iNode]
             currentXis = []
             for iFamilyMember in range(len(family)):
                 currentXis.append(xCoords[family[iFamilyMember]] - xCoords[iNode])
@@ -36,70 +50,90 @@ class PDDO:
         self.xis = xis
 
 
-    def calcSysMatrix(self, kappa, kappa2):
+    def calcSysMatrix(self):
+        kappa = self.kappa
+        kappa2 = self.kappa2
+        numNodes = self.numNodes
+        familyMembers = self.familyMembers
+        xis = self.xis
+        dx = self.dx
+        bVec = self.bVec
+        numBC = self.numBC
+        nodesBC = self.nodesBC
+        bVecBC = self.bVecBC
+
         #sysMatrix = np.zeros([self.numNodes + self.numBC,self.numNodes + self.numBC])
-        sysMatrix = np.zeros([self.numNodes,self.numNodes])
+        sysMatrix = np.zeros([numNodes, numNodes])
         
 
         #Left Boundary Condition Dirichlet 
         sysMatrix[0][0] = 1
         sysMatrix[0][1:] = 0
         #Differential Equation Part
-        for iNode in range(1,self.numNodes-1):
-            family = self.familyMembers[iNode]
-            xi = self.xis[iNode]
+        for iNode in range(1, numNodes-1):
+            family = familyMembers[iNode]
+            xi = xis[iNode]
             diffMat = np.zeros([3,3])
             for iFamilyMember in range(len(family)):
                 currentFamilyMember = family[iFamilyMember]
                 currentXi = xi[iFamilyMember]
                 pList = np.array([1, currentXi, (currentXi)**2])
                 weight = np.exp(-4*(np.absolute(currentXi))**2)
-                diffMat += weight*np.outer(pList,pList)*self.dx
+                diffMat += weight*np.outer(pList,pList)*dx
             for iFamilyMember in range(len(family)):
                 currentFamilyMember = family[iFamilyMember]
                 currentXi = xi[iFamilyMember]
                 pList = np.array([1, currentXi, (currentXi)**2])
                 weight = np.exp(-4*(np.absolute(currentXi))**2);
-                sysMatrix[iNode][ currentFamilyMember] = kappa*weight*np.inner(solve(diffMat,self.bVec),pList)*self.dx
+                sysMatrix[iNode][ currentFamilyMember] = kappa*weight*np.inner(solve(diffMat,bVec),pList)*dx
 
         #Boundary Condition
         #iNode = iNode -1
-        for iNodeBC in range(self.numBC):
+        for iNodeBC in range(numBC):
             iNode = iNode + 1
-            family = self.familyMembers[self.nodesBC[iNodeBC]]
-            xi = self.xis[self.nodesBC[iNodeBC]]
+            family = familyMembers[nodesBC[iNodeBC]]
+            xi = xis[nodesBC[iNodeBC]]
             diffMat = np.zeros([3,3])
             for iFamilyMember in range(len(family)):
                 currentFamilyMember = family[iFamilyMember]
                 currentXi = xi[iFamilyMember]
                 pList = np.array([1, currentXi, currentXi**2])
                 weight = np.exp(-4*(np.absolute(currentXi))**2)
-                diffMat += weight*np.outer(pList,pList)*self.dx
+                diffMat += weight*np.outer(pList,pList)*dx
             for iFamilyMember in range(len(family)):
                 currentFamilyMember = family[iFamilyMember]
                 currentXi = xi[iFamilyMember]
                 pList = np.array([1, currentXi, currentXi**2])
                 weight = np.exp(-4*(np.absolute(currentXi))**2);
-                sysMatrix[iNode][ currentFamilyMember] = kappa2*weight*np.inner(solve(diffMat,self.bVecBC),pList)*self.dx
-                #sysMatrix[ currentFamilyMember][iNode] = sysMatrix[iNode][ currentFamilyMember]
-                #a = input('').split(" ")[0]
+                sysMatrix[iNode][ currentFamilyMember] = kappa2*weight*np.inner(solve(diffMat,bVecBC),pList)*dx
         #sysMatrix[self.numNodes-1][self.numNodes-1] = 1
         #sysMatrix[0][1:] = 0 
         self.sysMatrix = sysMatrix
+    
+    def solve(self, tf, initialCondition):
+        numNodes = self.numNodes
+        dt = self.dt
+        BC = self.BC
+        numTimeSteps =int(tf/dt)
 
-    def adjustSysMatrixBoundaryNodes(self):
-        validValues = self.sysMatrix[3:10,6]
+        PDDO.findFamilyMembers(self)
+        PDDO.calcXis(self)
+        PDDO.calcSysMatrix(self)
+        sysMatrix = self.sysMatrix
         
-        self.sysMatrix[2:9,5] = validValues
-        self.sysMatrix[1:8,4] = validValues
-        self.sysMatrix[1:7,3] = validValues[1:7]
-        self.sysMatrix[1:6,2] = validValues[2:7]
-        self.sysMatrix[1:5,1] = validValues[3:7]
-        self.sysMatrix[1:4,0] = validValues[4:7]
+        identity = np.identity(numNodes-2)
+        sysMatrix[1:numNodes-1,0:numNodes] = np.multiply(dt,sysMatrix[1:numNodes-1,0:numNodes])
+        sysMatrix[1:numNodes-1,1:numNodes-1] = identity - sysMatrix[1:numNodes-1,1:numNodes-1]
+        KInvPDDO = inv(csc_matrix(sysMatrix)).toarray()
+        for iTimeStep in range(numTimeSteps):
+            initialCondition[0] = 0.0
+            initialCondition[-1] = BC[0]
+            initialCondition = np.matmul(KInvPDDO, initialCondition)
+            if iTimeStep == numTimeSteps-1:
+                SOL_PDDO = initialCondition
 
-        self.sysMatrix[self.numNodes-9:self.numNodes-2,self.numNodes-6] = validValues
-        self.sysMatrix[self.numNodes-8:self.numNodes-1,self.numNodes-5] = validValues
-        self.sysMatrix[self.numNodes-7:self.numNodes-1,self.numNodes-4] = validValues[0:6]
-        self.sysMatrix[self.numNodes-6:self.numNodes-1,self.numNodes-3] = validValues[0:5]
-        self.sysMatrix[self.numNodes-5:self.numNodes-1,self.numNodes-2] = validValues[0:4]
-        self.sysMatrix[self.numNodes-4:self.numNodes-1,self.numNodes-1] = validValues[0:3]
+        #print('Here')
+        #a = input('').split(" ")[0]
+        #pddo.adjustSysMatrixBoundaryNodes()
+
+        self.SOL_PDDO = SOL_PDDO
